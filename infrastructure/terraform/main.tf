d5d4d42db2d65687781ae6d9440f6a11c712e384
fdngdf <165413554+Zhaka0110@users.noreply.github.com>
2026-02-11 12:15:07 +0600

    feat(terraform): create project.txt via local_file

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,3 +1,5 @@
+
+
 terraform {
   required_version = ">= 1.5.0"
 }
@@ -5,7 +7,10 @@ terraform {
 locals {
   name = "${var.project_name}-${var.environment}"
 }
-
+resource "local_file" "project_info" {
+  filename = "${path.module}/project.txt"
+  content  = "Project: ${local.name}\n"
+}
 output "name" {
   value = local.name
 }
