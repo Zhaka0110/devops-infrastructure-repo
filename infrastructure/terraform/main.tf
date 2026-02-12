5d0bc567a9f61fed744e5bbd0dfc8652af72b90a
fdngdf <165413554+Zhaka0110@users.noreply.github.com>
2026-02-12 08:46:57 +0600

    ci: move workflows to root .github/workflows

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -8,8 +8,10 @@ locals {
   name = "${var.project_name}-${var.environment}"
 }
 resource "local_file" "project_info" {
-  filename = "${path.module}/project.txt"
-  content  = "Project: ${local.name}\n"
+  filename             = "${path.module}/project.txt"
+  content              = "Project: ${local.name}\n"
+  file_permission      = "0644"
+  directory_permission = "0755"
 }
 output "name" {
   value = local.name
